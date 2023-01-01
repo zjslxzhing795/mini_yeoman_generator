0ed487606ae30a43c59bf4d403b40982d0b4dd41
grace <1027502235@qq.com>
2023-01-01 16:33:17 +0800

    feat(generator): 使用prompting接收用户输入

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,28 @@
 const Generator = require("yeoman-generator")
 
 module.exports = class extends Generator {
+  prompting() {
+    // Yeoman 在询问用户环节会自动调用此方法
+    // 在此方法中可以调用父类的prompt()方法发出对用户的命令行询问
+    return this.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Your project title",
+        default: this.appname, // appname 为项目生成目录名称
+      },
+      {
+        type: "input",
+        name: "success",
+        message: "Your project status",
+        default: true,
+      },
+    ]).then((answers) => {
+      // answers => {name: 'user input value'}
+      console.log("answers=", answers)
+      this.answer = answers
+    })
+  }
   writing() {
     // Yeoman自动生成文件阶段调用此方法
     // 我们尝试往项目目录中写入文件
@@ -11,10 +33,11 @@ module.exports = class extends Generator {
     // 输出目标路径
     const output = this.destinationPath("foofoofoo.txt")
     // 模版数据上下文
-    const context = {
-      title: "Hello, zz",
-      success: true,
-    }
+    const context = this.answer
+    // {
+    //   title: "Hello, zz",
+    //   success: true,
+    // }
     this.fs.copyTpl(tmpl, output, context)
   }
 }
